e230c308b024986dff5dc1ebbb9493a75fed8f16
Andrew Accuardi <andrewaccuardi@gmail.com>
2016-12-02 09:26:32 -0800

    Add routing for index erb

--- app.rb
+++ app.rb
@@ -3,16 +3,17 @@ require('sinatra/reloader')
 require('pry')
 require('capybara')
 require('./lib/word')
-require('.lib/definition')
+require('./lib/definition')
 also_reload('lib/**/*.rb')
 
-get('/') do
+get ('/') do
   @words = Word.all()
   erb(:index)
 end
 
-post('/') do
+post ('/') do
   word = params.fetch("inputted_word")
-  Word.new(:word => word)
+  Word.new(:word => word).save()
+  @words = Word.all()
   erb(:index)
 end
