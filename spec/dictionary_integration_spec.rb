bb171d9d54aa399f58ca2d59563f7e32ef3f8c0b
Andrew Accuardi <andrewaccuardi@gmail.com>
2016-12-02 10:38:21 -0800

    Add passing integration spec for taking the user to the add definition page

--- spec/dictionary_integration_spec.rb
+++ spec/dictionary_integration_spec.rb
@@ -12,3 +12,16 @@ describe("The add word path", {:type => :feature}) do
     expect(page).to have_content("Dog")
   end
 end
+describe("The add definition page path", {:type => :feature}) do
+  it("Takes the user to the add definition page") do
+    visit('/')
+    fill_in('inputted_word', :with => "Cat")
+    click_on("Submit Your Word")
+    click_on("Cat")
+    expect(page).to have_content("Cat")
+  end
+end
+
+# fill_in("definition", :with => "Couch killing machine")
+# click_on("Add Definition")
+# expect(page).to have_content("Couch killing machine")
